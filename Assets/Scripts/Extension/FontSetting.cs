c977a095bf378310a11e6c26d486bd4116d85f33
wata548 <watashimokia@gmail.com>
2025-12-26 00:04:33 +0900

    Add Font Setting

---
+++ Assets/Scripts/Extension/FontSetting.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Lang;
+using TMPro;
+using UnityEngine;
+
+namespace Extension {
+
+    [Serializable]
+    public struct LanguageFontMatch {
+        public Language Language;
+        public TMP_FontAsset Font;
+    }
+    
+    [CreateAssetMenu(menuName = "Font/Setting")]
+    public class FontSetting: ScriptableObject {
+        [SerializeField] 
+        private List<LanguageFontMatch> _fonts;
+
+        public void Init() {
+            foreach (var match in _fonts) {
+                TMP_LangText.SetFont(match.Language, match.Font);
+            }
+        }
+    }
+}
